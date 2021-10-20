6c138bdc56044242cfe22a5119298f2ba71a77aa
jmalone801 <jmalone801@gmail.com>
2021-10-19 20:23:55 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function like1(){
+    document.querySelector("#likeAmount1").innerText++;
+}
+
+function like2(){
+    document.querySelector("#likeAmount2").innerText++;
+}
+
+function like3(){
+    document.querySelector("#likeAmount3").innerText++;
+}
